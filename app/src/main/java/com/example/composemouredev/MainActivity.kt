1921675720827f8d895a0f83d8a037c70a2b6c1e
andresrubianoch <andres.rubiano.ch@gmail.com>
2023-02-16 14:36:10 -0500

    Modifier size, clip, background

--- app/src/main/java/com/example/composemouredev/MainActivity.kt
+++ app/src/main/java/com/example/composemouredev/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +47,11 @@ fun MyTexts(text: String) {
 fun MyImage() {
     Image(
         painter = painterResource(R.drawable.ic_launcher_foreground),
-        contentDescription = "Prueba imagen"
+        contentDescription = "Prueba imagen",
+        modifier = Modifier
+            .size(64.dp)
+            .clip(CircleShape)
+            .background(Color.Gray)
     )
 }
 
