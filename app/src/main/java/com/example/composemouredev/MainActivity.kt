b197dbc68412fa18d5ddc172ef0c72319fa72e2a
andresrubianoch <andres.rubiano.ch@gmail.com>
2023-02-16 14:21:38 -0500

    Image and Row

--- app/src/main/java/com/example/composemouredev/MainActivity.kt
+++ app/src/main/java/com/example/composemouredev/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.composemouredev
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -13,21 +16,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTexts("Hello, Emi!")
+            MyComponentAsRow()
         }
     }
+}
 
-    @Composable
-    fun MyTexts(text: String) {
-        Column() {
-            Text(text)
-            Text("Te amamos con el alma")
-        }
+@Composable
+fun MyComponentAsRow() {
+    Row {
+        MyImage()
+        MyTexts(text = "Hello, Emi")
     }
+}
 
-    @Preview
-    @Composable
-    fun PreviewTexts() {
-        MyTexts(text = "Hello, Emi.")
+@Composable
+fun MyTexts(text: String) {
+    Column {
+        Text(text)
+        Text("Te amamos con el alma")
     }
+}
+
+@Composable
+fun MyImage() {
+    Image(
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = "Prueba imagen"
+    )
+}
+
+@Preview
+@Composable
+fun PreviewTexts() {
+    MyComponentAsRow()
 }
