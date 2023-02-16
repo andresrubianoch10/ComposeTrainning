e5c200df6f31cdd30f022f3fce67feae8276fdfd
andresrubianoch <andres.rubiano.ch@gmail.com>
2023-02-16 13:40:40 -0500

    setContent, @Preview, @Composable, Text()

--- app/src/main/java/com/example/composemouredev/MainActivity.kt
+++ app/src/main/java/com/example/composemouredev/MainActivity.kt
@@ -3,15 +3,27 @@ package com.example.composemouredev
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composemouredev.ui.theme.ComposeMoureDevTheme
 
 class MainActivity : ComponentActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyText("Hello, Emi!")
+        }
+    }
+
+    @Composable
+    fun MyText(text: String) {
+        Text(text)
+    }
+
+    @Preview
+    @Composable
+    fun PreviewTexts() {
+        MyText(text = "Hello, Emi.")
+    }
 }
