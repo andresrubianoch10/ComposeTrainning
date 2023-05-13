e049abe21aa91a0522340875278f10acd6340f2f
andresrubianoch <andres.rubiano.ch@gmail.com>
2023-05-12 20:07:48 -0500

    Now, with this change is working but we are using the correct access method from mutable state of

--- app/src/main/java/com/example/composemouredev/RecompositionScreen.kt
+++ app/src/main/java/com/example/composemouredev/RecompositionScreen.kt
@@ -6,19 +6,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MyStateExample() {
-    var counter = 0
+    var counter = remember { mutableStateOf(0) }
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { counter++ }) {
+        Button(onClick = { counter.value += 1 }) {
             Text(text = "Pulsar")
         }
         Text(text = "He sido pulsado $counter veces")
